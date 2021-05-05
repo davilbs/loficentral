1c9cbc674ee7a3c5f3a537362637f146e2184294
Marcos Samuel <m.samuel43.yahoo@gmail.com>
2021-05-04 22:45:21 -0300

    separar paginas

---
+++ src/components/ListRadios.jsx
@@ -0,0 +1,20 @@
+import logo from './../logo.svg';
+
+function ListRadios() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >LIST RADIOS</a>
+            </header>
+        </div>
+    )
+}
+
+export default ListRadios;
